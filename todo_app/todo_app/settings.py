4c2a264482408ad18dc437bee1203984810af113
jh31barbosa <vazavult@gmail.com>
2022-10-30 20:16:59 -0300

    deploy

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$7$**3$riob@-vc&85w(4pn+p*c%ez#ap6&tcj*+d-7!!6#^t!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
