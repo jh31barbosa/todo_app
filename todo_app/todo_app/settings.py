52dcf0d5163615163e4c74829af3cc2d9592d567
jh31barbosa <vazavult@gmail.com>
2022-10-30 21:08:36 -0300

    s

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$7$**3$riob@-vc&85w(4pn+p*c%ez#ap6&tcj*+d-7!!6#^t!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
